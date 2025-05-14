486558b022da69b074ae9f8b7ad4f29fd103b2cf
Vicen621 <71749981+vicen621@users.noreply.github.com>
2025-05-14 09:46:48 -0300

    reversed test commit

--- Refactoring/Ejercicio5/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/GestorNumerosDisponibles.java
+++ Refactoring/Ejercicio5/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/GestorNumerosDisponibles.java
@@ -7,7 +7,6 @@ import java.util.TreeSet;
 
 public class GestorNumerosDisponibles {
     private final SortedSet<String> lineas = new TreeSet<>();
-	// test
     private String tipoGenerador = "ultimo";
 
     public SortedSet<String> getLineas() {
