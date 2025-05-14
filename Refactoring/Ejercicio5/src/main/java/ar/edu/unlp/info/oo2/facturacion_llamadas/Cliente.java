81221ed8897021f68185194ae334c350766ca490
Vicen621 <71749981+vicen621@users.noreply.github.com>
2025-05-14 16:22:38 -0300

    move method calcularMontoTotalLlamadas -> Cliente

--- Refactoring/Ejercicio5/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Cliente.java
+++ Refactoring/Ejercicio5/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Cliente.java
@@ -49,4 +49,26 @@ public class Cliente {
 	public void setLlamadas(List<Llamada> llamadas) {
 		this.llamadas = llamadas;
 	}
+
+	public double calcularMontoTotalLlamadas() {
+		double c = 0;
+		for (Llamada l : getLlamadas()) {
+			double auxc = 0;
+			if (l.getTipoDeLlamada() == "nacional") {
+				// el precio es de 3 pesos por segundo más IVA sin adicional por establecer la llamada
+				auxc += l.getDuracion() * 3 + (l.getDuracion() * 3 * 0.21);
+			} else if (l.getTipoDeLlamada() == "internacional") {
+				// el precio es de 150 pesos por segundo más IVA más 50 pesos por establecer la llamada
+				auxc += l.getDuracion() * 150 + (l.getDuracion() * 150 * 0.21) + 50;
+			}
+
+			if (getTipo() == "fisica") {
+				auxc -= auxc* Empresa.descuentoFis;
+			} else if(getTipo() == "juridica") {
+				auxc -= auxc* Empresa.descuentoJur;
+			}
+			c += auxc;
+		}
+		return c;
+	}
 }
