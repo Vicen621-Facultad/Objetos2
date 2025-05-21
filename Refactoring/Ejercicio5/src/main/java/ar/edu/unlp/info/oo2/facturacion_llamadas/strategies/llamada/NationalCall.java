e5ffb7ee4ba04eb4edb35eba0271862e67f13fdc
Vicen621 <71749981+vicen621@users.noreply.github.com>
2025-05-21 15:12:25 -0300

    replace magic literal

--- Refactoring/Ejercicio5/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/strategies/llamada/NationalCall.java
+++ Refactoring/Ejercicio5/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/strategies/llamada/NationalCall.java
@@ -2,9 +2,12 @@ package ar.edu.unlp.info.oo2.facturacion_llamadas.strategies.llamada;
 
 //TODO: magic numbers
 public class NationalCall implements PrecioStrategy {
+
+    public static final int PRECIO_POR_SEGUNDO = 3;
+
     @Override
     public double calcularPrecio(int duracion) {
         // el precio es de 3 pesos por segundo más IVA
-        return duracion * 3 + (duracion * 3 * IVA);
+        return duracion * PRECIO_POR_SEGUNDO + (duracion * PRECIO_POR_SEGUNDO * IVA);
     }
 }
