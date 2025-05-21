4f48e5204d5ff1b78dc8eca67181a8830a5609cc
Vicen621 <71749981+vicen621@users.noreply.github.com>
2025-05-21 15:12:43 -0300

    replace magic literal

--- Refactoring/Ejercicio5/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/strategies/llamada/InternationalCall.java
+++ Refactoring/Ejercicio5/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/strategies/llamada/InternationalCall.java
@@ -2,9 +2,11 @@ package ar.edu.unlp.info.oo2.facturacion_llamadas.strategies.llamada;
 
 public class InternationalCall implements PrecioStrategy {
 
+    public static final int PRECIO_POR_SEGUNDO = 150;
+
     @Override
     public double calcularPrecio(int duracion) {
         // el precio es de 150 pesos por segundo más IVA más 50 pesos por establecer la llamada
-        return duracion * 150 + (duracion * 150 * IVA) + 50;
+        return duracion * PRECIO_POR_SEGUNDO + (duracion * PRECIO_POR_SEGUNDO * IVA) + 50;
     }
 }
